411aca5da843258caf65dbf2462d3a05d6cf9c3b
Santiago Forero <saforeros@unal.edu.co>
2025-01-26 10:52:07 -0500

    add exceptions to 4th

--- 4_mayor_suma_consecutivos.py
+++ 4_mayor_suma_consecutivos.py
@@ -1,6 +1,12 @@
 # Escribir una función que reciba una lista de números enteros y retorne la mayor suma entre dos elementos consecutivos.
 
 def mayor_suma_consecutivos(lista):
+
+    if not isinstance(lista, list):
+        raise ValueError("El valor ingresado no es una lista.")
+    if not all(isinstance(n, int) for n in lista):
+        raise ValueError("La lista debe contener solo números enteros.")
+
     suma_mayor = lista[0] + lista[1]
 
     for i in range(1, len(lista) - 1):
@@ -12,3 +18,8 @@ def mayor_suma_consecutivos(lista):
 
 # Ejemplo de uso
 print(mayor_suma_consecutivos([1,2, 3, 5, 10, 6, 11 ]))
+
+try:
+    print(mayor_suma_consecutivos([1, 2, 3, 4, 5, 6, 7, 8, 9, "10"]))
+except ValueError as exc:
+    print(exc)
