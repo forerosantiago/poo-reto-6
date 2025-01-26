cd0c46f8273b487fffbb0d39275448c9253781dd
Santiago Forero <saforeros@unal.edu.co>
2025-01-26 10:47:46 -0500

    add exceptions to palindromo

--- 2_palíndromo.py
+++ 2_palíndromo.py
@@ -1,4 +1,9 @@
 def es_palindromo(palabra):
+    if not isinstance(palabra, str):
+        raise ValueError("El valor ingresado no es una cadena de texto.")
+    if palabra == "":
+        raise ValueError("La cadena de texto está vacía.")
+
     palabra = palabra.lower()
 
     # Se compara el primer carácter con el último, luego el segundo con el penúltimo y así...
@@ -8,8 +13,18 @@ def es_palindromo(palabra):
     return True
 
 # Ejemplo usando la función
-palabra = input("Ingrese una palabra para verificar si es un palíndromo: ")
-if es_palindromo(palabra):
+word = input("Ingrese una palabra para verificar si es un palíndromo: ")
+if es_palindromo(word):
     print("La palabra es un palíndromo.")
 else:
     print("La palabra NO es un palíndromo.")
+
+try:
+    es_palindromo("")
+except ValueError as exc:
+    print(exc)
+
+try:
+    es_palindromo(13.45)
+except ValueError as exc:
+    print(exc)
